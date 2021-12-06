6990defa784b2adfe72b1d83c95f8484bb48db53
HuaZou <zouhua1@outlook.com>
2021-12-06 12:50:18 +0800

    update data

--- R/Data.R
+++ R/Data.R
@@ -50,3 +50,36 @@
 # roxygen2::roxygenise("D:/Project/MyRtools/")
 # devtools::check(document = FALSE)
 # devtools::build()
+
+# building dataset
+# Profile <- data.table::fread(system.file("extdata", "Species_relative_abundance.tsv", package="MyRtools"))  %>% tibble::column_to_rownames("V1")
+# Metadata <- read.csv(system.file("extdata", "Metadata.csv", package="MyRtools"))
+# Feature <- read.csv(system.file("extdata", "Species_feature.csv", package="MyRtools")) %>% tibble::column_to_rownames("Species")
+#
+# ExprSet_species <- get_ExprSet(profile=Profile,
+#                                metadata=Metadata,
+#                                feature=Feature,
+#                                trim=TRUE,
+#                                occ_Feature=0.2,
+#                                each=TRUE,
+#                                occ_Sample=0.2)
+# save(ExprSet_species, file = "data/ExprSet_species.rda")
+# Profile <- Profile * 10e6
+# ExprSet_species_count <- get_ExprSet(profile=Profile,
+#                                      metadata=Metadata,
+#                                      feature=Feature,
+#                                      trim=TRUE,
+#                                      occ_Feature=0.2,
+#                                      each=TRUE,
+#                                      occ_Sample=0.2)
+# save(ExprSet_species_count, file = "data/ExprSet_species_count.rda")
+#
+# PROF <- Profile_table(as.matrix(Profile), feature_are_row=TRUE)
+# META <- Sample_data(Metadata)
+# FEATURE <- Feature_table(Feature)
+#
+# mydataset <- MyDataSet(Profile_table=PROF,
+#                        Sample_data=META,
+#                        Feature_table=FEATURE)
+# save(mydataset, file = "data/MyDataSet.rda")
+#
